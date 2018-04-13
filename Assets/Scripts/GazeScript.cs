94d1732daa00ea7c1b3ebea986a36f029fbf41f2
lukas fritz <lukas.fritz@hotmail.com>
2018-04-13 19:01:10 +0300

    added gaze puzzle + sound + diverse

---
+++ Assets/Scripts/GazeScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeScript : MonoBehaviour {
+
+    public float sightlength = 100f;
+    public GameObject selectedObj;
+    public float hoverSpeed = 0.5f;
+    public GameObject cub1;
+    public GameObject cub2;
+    public AudioClip jingle;
+    private AudioSource audioSource { get { return GetComponent<AudioSource>(); } }
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = jingle;
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        RaycastHit seen;
+        Ray raydirection = new Ray(transform.position, transform.forward);
+        if(Physics.Raycast(raydirection, out seen, sightlength))
+        {
+            if (seen.collider.tag == "Button")
+            {
+                if ((cub1.active == false)&&(cub2.active ==false)) {
+                    audioSource.PlayOneShot(jingle, 0.7F);
+                    //  Debug.Log("found");
+                    cub1.SetActive(true);
+                cub2.SetActive(true);
+                }
+                /*    if(selectedObj!= null && selectedObj != seen.transform.gameObject)
+                    {
+                                GameObject hitObject = seen.transform.gameObject;
+                              MoveMenuButton(hitObject);
+
+                    }
+                    selectedObj = seen.transform.gameObject;*/
+            }
+        }
+	}
+    private void MoveMenuButton(GameObject hitObject)
+    {
+        Debug.Log("found");
+     //   cubi.SetActive(true);
+        Vector3 newZ = hitObject.transform.position;
+        newZ.z -= hoverSpeed;
+        selectedObj.transform.position = newZ;
+
+        newZ.z += hoverSpeed * 2;
+        hitObject.transform.position = newZ;
+    }
+}
--- Assets/winscript.cs
+++ Assets/winscript.cs
@@ -17,7 +17,7 @@ public class winscript : MonoBehaviour {
     void Start () {
        target1 = new Vector3(-4.326f, 0.88f, -0.379f);
        t1_rot = new Vector3(0, 90.0f, 0);
-        target2 = new Vector3(-4.305f, 0.88f, 0.129f);
+       target2 = new Vector3(-4.305f, 0.88f, 0.129f);
        t2_rot = new Vector3(0, 0, 90.0f);
     }
 	
@@ -25,15 +25,39 @@ public class winscript : MonoBehaviour {
 	void Update () {
         if (Vector3.Distance(cube1.transform.position, target1) < 0.1)
         {
-             if (Vector3.Angle(cube1.transform.rotation.eulerAngles, t1_rot) < 0.1)
+           //  if (Vector3.Angle(cube1.transform.rotation.eulerAngles, t1_rot) < 0.1)
             {
-                if (Vector3.Distance(cube2.transform.position, target2) < 0.1)
+                float tmp = Vector3.Angle(cube1.transform.forward, Vector3.right);
+                float tmp1 = Vector3.Angle(cube1.transform.forward, Vector3.up);
+                float tmp2 = Vector3.Angle(cube1.transform.forward, Vector3.forward);
+                // float tmp = Vector3.Angle(cube1.transform.position, Vector3.right);
+                Debug.Log("Step 1: "+ tmp2);
+                if ((tmp > -2.0f) &&(tmp < 2.0f) && (88.0f < tmp1) && (tmp1 < 92.0f))
                 {
-                    if (Vector3.Angle(cube2.transform.rotation.eulerAngles, t2_rot) < 0.01)
+                    Debug.Log("Cube1 correct: " + tmp2);
+                    if (Vector3.Distance(cube2.transform.position, target2) < 0.1)
                     {
-                        podest.SetActive(false);
-                        GetComponent<Renderer>().material.mainTexture = textl;
-                        Debug.Log("You win");
+                        float tmp3 = Vector3.Angle(cube2.transform.forward, Vector3.right);
+                        float tmp4 = Vector3.Angle(cube2.transform.forward, Vector3.up);
+                        float tmp5 = Vector3.Angle(cube2.transform.forward, Vector3.forward);
+                        // float tmp = Vector3.Angle(cube1.transform.position, Vector3.right);
+                           Debug.Log("Step2: "+ tmp3+"temp4: "+tmp4);
+                        if ((88.0f < tmp3) && (tmp3 < 92.0f) && (88.0f < tmp4) && (tmp4 < 92.0f))
+                        {
+
+                    //        if (Vector3.SignedAngle(cube2.transform.rotation.eulerAngles, t2_rot, Vector3.up) < 0.1)
+                     //   {
+                            podest.SetActive(false);
+                            GetComponent<Renderer>().material.mainTexture = textl;
+                            Debug.Log("You win");
+
+                            //TODO print values for check
+                            // angle_z = Vector3.Angle(transform.up, distance);
+                            //  angle_x = Vector3.Angle(transform.right, distance);
+                            //   angle_y = Vector3.Angle(transform.forward, distance);
+                            //   Vector3.SignedAngle();
+                            //
+                        }
                     }
                 }
             }
